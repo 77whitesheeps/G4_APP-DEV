ce1f58ea98bfdce27926bc29973ce4c52f15c72a
77whitesheeps <00keandra.acosta@gmail.com>
2025-09-02 16:58:58 +0800

    Fixed route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlantingCalculatorController;
 
+Route::get('/', function () {
+    return redirect()->route('planting.calculator');
+});
+
 Route::get('/planting-calculator', [PlantingCalculatorController::class, 'index'])->name('planting.calculator');
 Route::post('/calculate-plants', [PlantingCalculatorController::class, 'calculate'])->name('calculate.plants');
